44f245f5c63edbc5f70fcfcaec0577b72916f6db
rhldkszns97 <sujeng98@naver.com>
2022-08-26 00:55:20 +0900

    다 삭제하고 f1부터 시작

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -9,73 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TodoController {
 
-    @GetMapping("/")
-    public String getIndex() {
-        return "To-do Apllication!";
-    }
-
     @GetMapping("/1")
-    public String getIndex1() {
-        return "To-do Application 1!";
+    public String f1() {
+        return "f1";
     }
 
-    @GetMapping("/feat/1")
-    public String feat1() {
-        return "feat1";
-    }
-
-
-    @GetMapping("/feat/2")
-    public String feat2() {
-        return "feat2";
-
-    }
-
-    @GetMapping("/feat/2-1")
-    public String feat21() {
-        return "feat2-1";
-    }
-    
-    @GetMapping("/main/1")
-    public String main1() {
-        return "meat1";
-    }
-
-
-    @GetMapping("/main/11")
-    public String main11() {
-        return "meat11";
-    }
-
-    @GetMapping("/main/22")
-    public String main22() {
-        return "meat22";
-    }
-
-    @GetMapping("/main/33")
-    public String main33() {
-        return "meat33";
-    }
-
-    @GetMapping("/main/44")
-    public String main44() {
-        return "meat44";
-
-    }
-
-    @GetMapping("/main/55")
-    public String main55() {
-        return "meat55";
-    }
-
-    @GetMapping("/main/66")
-    public String main66() {
-        return "meat66";
-    }
-
-    @GetMapping("/main/77")
-    public String main77() {
-        return "meat77";
-    }
 
 }
