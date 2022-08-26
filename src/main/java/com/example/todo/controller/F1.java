9e6aebcf09490913ff1493ace8220802e124b10f
rhldkszns97 <sujeng98@naver.com>
2022-08-26 15:55:16 +0900

    f3추가

--- src/main/java/com/example/todo/controller/F1.java
+++ src/main/java/com/example/todo/controller/F1.java
@@ -3,5 +3,6 @@ package com.example.todo.controller;
 public class F1 {
     public static void main(String[] args) {
         System.out.println("f1추가!!");
+        System.out.println("f3추가!!");
     }
 }
