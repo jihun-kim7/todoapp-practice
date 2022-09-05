7d1b30ec5c58b85b091952097f7941cf0c6d7c98
rhldkszns97 <sujeng98@naver.com>
2022-09-05 13:40:34 +0900

    c1

--- src/main/java/com/example/todo/controller/A1.java
+++ src/main/java/com/example/todo/controller/A1.java
@@ -2,6 +2,5 @@ package com.example.todo.controller;
 
 public class A1 {
     public static void main(String[] args) {
-        System.out.println("a1");
     }
 }
--- src/main/java/com/example/todo/controller/B1.java
+++ src/main/java/com/example/todo/controller/B1.java
@@ -2,6 +2,5 @@ package com.example.todo.controller;
 
 public class B1 {
     public static void main(String[] args) {
-        System.out.println("b1");
     }
 }
