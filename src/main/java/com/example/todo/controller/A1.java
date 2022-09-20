cb5e7fbf4df9e403c6907e88c4a035cd00c0f2bb
rhldkszns97 <sujeng98@naver.com>
2022-09-20 22:51:34 +0900

    delete

--- src/main/java/com/example/todo/controller/A1.java
+++
@@ -1,6 +0,0 @@
-package com.example.todo.controller;
-
-public class A1 {
-    public static void main(String[] args) {
-    }
-}
--- src/main/java/com/example/todo/controller/B1.java
+++
@@ -1,6 +0,0 @@
-package com.example.todo.controller;
-
-public class B1 {
-    public static void main(String[] args) {
-    }
-}
